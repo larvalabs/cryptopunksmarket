b35b9a03a65496db739db4f92edd995581fff6a5
Helmi Nur Ihsan <helmi.n.ihsan@gmail.com>
2022-02-10 02:50:12 +0700

    add compiled static js script

---
+++ js/udomain.js
@@ -0,0 +1,9 @@
+var UDomain = {};
+
+UDomain.init = function() {
+  window.uauth = initUDomain(
+    'IkYioPn2RAV2lOSYtZMU9JmrWTXqd1FWXXR8VqEjsbk=',
+    'ZLsn2pPoEd4n5+EG1CWfVKktRUOu4QuqVFZg+gPyZa4=',
+    'https://hnihsan-cryptopunksmarket.vercel.app/'
+  );
+}
